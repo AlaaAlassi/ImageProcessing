0b3f610532958c3ee685638a9a984215c097ea42
Alaa <alaa.alasi@yahoo.com>
2016-10-23 19:22:46 +0300

    Adding Comments

--- hough_function.m
+++ hough_function.m
@@ -7,18 +7,31 @@ rho = (-rho_limit:1:rho_limit);
 theta = (0:theta_sample_frequency:pi);
 num_thetas = numel(theta);
 num_rhos = numel(rho);
+
+% NOTE : the rows represenrs the length and columns represents the angle  
 hough_space = zeros(num_rhos, num_thetas);
 
 % perform hough transform
 for xi = 1:x
     for yj = 1:y
+        
+        %enter te loop f a whight pixel is detected 
         if image(xi, yj) == 1 
-            for theta_index = 1:num_thetas
-                th = theta(theta_index);
+            
+            %iterate through the theta vector 
+            for theta_i = 1:num_thetas
+                %store the value of theta in a separate variable 
+                th = theta(theta_i);
+                %This forumla generates the values of the posible
+                %lines that passes through the pixel (xi,yi) remeber that 
+                %the r is just the length of vector that is perpendicular
+                %to the line passing through the pixel (the line we are
+                %voting for )
                 r  = xi * cos(th) + yj * sin(th);
-                rho_index = round(r + num_rhos/2);                      
-                hough_space(rho_index, theta_index) = ...
-                     hough_space(rho_index, theta_index) + 1;
+                %this va
+                rho_index = round(r+num_rhos/2);        %num_rhos/2              
+                hough_space(rho_index, theta_i) = ...
+                     hough_space(rho_index, theta_i) + 1;
             end
         end
     end
