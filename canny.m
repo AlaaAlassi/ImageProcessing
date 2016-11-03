bab7d134288b362685234f110463549fcbedb0b3
Alaa <alaa.alasi@yahoo.com>
2016-11-03 22:24:48 +0200

    finished but needs cleaning

--- canny.m
+++ canny.m
@@ -41,7 +41,7 @@ function outimage = canny(imageIN)
     
     for v = 1 : y
       for u = 1 : x
-        theta(u,v) = abs(atand(Iyy(u,v)/Ixx(u,v))); 
+        theta(u,v) = abs(atand(Iyy(u,v)/(Ixx(u,v)+0.01))); 
         
          if ((theta(u,v) >= 0) && (theta(u,v) <= 22.5) || (theta(u,v) >= 157.5) && (theta(u,v) <= 180))
             ntheta(u,v) = 0;
@@ -65,72 +65,76 @@ function outimage = canny(imageIN)
 
     
  
-
+for v = 2 : y-1
+      for u = 2 : x-1
     
 
-%         %     N O N - M A X I M U M   S U P P R E S S I O N
-%         if (ntheta(u,v) == 0)
-%                 if (mod(u, v) < mod(u-1, v) || mod(u, v) < mod(u+1, v))
-%                     supimg(u,v) = 0;
-%                 else supimg(u,v)= mod(u,v);
-%                 end; 
-%             end;
-%                 
-%             
-%             if (ntheta(u,v) == 45)
-%                 if (mod(u, v) < mod(u+1, v-1) || mod(u, v) < mod(u-1, v+1))
-%                     supimg(u,v) = 0;
-%                 else supimg(u,v)= mod(u,v);
-%                 end;
-%             end;
-%                 
-%             
-%             if (ntheta(u,v) == 90)
-%                 if (mod(u, v) < mod(u, v-1) || mod(u, v) < mod(u, v+1))
-%                     supimg(u,v) = 0;
-%                 else supimg(u,v)= mod(u,v);
-%                 end;
-%             end;
-%                
-%             if (ntheta(u,v) == 135)
-%                 if (mod(u, v) < mod(u-1, v-1) || mod(u, v) < mod(u+1, v+1))
-%                     supimg(u,v) = 0;
-%                 else supimg(u,v) = mod(u,v);%Supimg -- Image obtained after Non-mSupression
-%                 end ;
-%             end;
-%                 
-%             %    E N D  O F  N O N - M A X I M U M   S U P P R E S S I O N
-%     end
-% end
-% 
+        %     N O N - M A X I M U M   S U P P R E S S I O N
+        if (ntheta(u,v) == 0)
+                if (mod(u, v) < mod(u-1, v) || mod(u, v) < mod(u+1, v))
+                    supimg(u,v) = 0;
+                else supimg(u,v)= mod(u,v);
+                end
+        end
+                
+            
+            if (ntheta(u,v) == 45)
+                if (mod(u, v) < mod(u+1, v-1) || mod(u, v) < mod(u-1, v+1))
+                    supimg(u,v) = 0;
+                else supimg(u,v)= mod(u,v);
+                end
+            end
+                
+            
+            if (ntheta(u,v) == 90)
+                if (mod(u, v) < mod(u, v-1) || mod(u, v) < mod(u, v+1))
+                    supimg(u,v) = 0;
+                else supimg(u,v)= mod(u,v);
+                end
+            end
+               
+            if (ntheta(u,v) == 135)
+                if (mod(u, v) < mod(u-1, v-1) || mod(u, v) < mod(u+1, v+1))
+                    supimg(u,v) = 0;
+                else supimg(u,v) = mod(u,v);%Supimg -- Image obtained after Non-mSupression
+                end 
+            end
+           
+       end
+end
+ 
+
+
+%   %    E N D  O F  N O N - M A X I M U M   S U P P R E S S I O N
 % 
 % %T H R E S H O L D I N G
-% 
-% th = 8.0;   %High Threshhold
-% %th = max(supimg);
-% tl = th/6; %Low Threshhold
-% gnh(u,v) = 0;
-% gnl(u,v) = 0;
-% for v = 2 : 509
-%     for u = 2 : 509
-%         if(supimg(u,v) >= th)
-%             gnh(u,v) = supimg(u,v);
-%         end
-%         if(supimg(u,v) >= tl)
-%             gnl(u,v) = supimg(u,v);
-%         end
-%      
-%     end
-%     
-% end
+
+th = 0.8;   %High Threshhold 8
+%th = max(supimg);
+tl = th/6; %Low Threshhold
+gnh(x,y) = 0;
+gnl(x,y) = 0;
+for v = 2 : y-1
+    for u = 2 :x-1
+        if(supimg(u,v) >= th)
+            gnh(u,v) = supimg(u,v);
+        end
+        if(supimg(u,v) >= tl)
+            gnl(u,v) = supimg(u,v);
+        end
+     
+    end
+    
+end
+
 % E N D  OF T H R E S H O L D I N G
-%resimg = gnl - gnh;   %Edge of the Image 
+resimg = gnl - gnh;   %Edge of the Image 
    
 %subplot (1, 2, 1),imshow(ID);axis image; title('Original Image');
-%subplot (1, 2, 2),imshow(resimg);axis image; title('Canny Edge Image');
+ subplot (1, 2, 2),imshow(resimg);axis image; title('Canny Edge Image');
 
 
-outimage =  ntheta ; %resimg
+outimage = resimg; %resimg
 
 
 %imshow(resimg);
