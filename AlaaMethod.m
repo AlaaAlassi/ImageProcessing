90db3b61e315feab1419a83cb5bf485c44f72894
Alaa <alaa.alasi@yahoo.com>
2017-01-19 19:39:57 +0200

    to be cont

--- AlaaMethod.m
+++ AlaaMethod.m
@@ -22,6 +22,9 @@ axis on, axis normal, hold on;
 % pay attention to 'NHoodSize' if it is large some lines with same fetures  will be consedered
 % as one 
  P  = houghpeaks(H,100,'threshold',0.5*max(H(:)), 'NHoodSize',[13  315]);
+ 
+
+ 
 x = T(P(:,2)); y = R(P(:,1));
 plot(x,y,'s','color','white');
 
@@ -62,10 +65,34 @@ for k = 1:length(centersSorting);
         A(m,k) =  LinesHeight(m) - centersSorting(k,2);
     end
      if A(1,k) >= -60 && A(1,k)<= -55
-        disp('c')  
+      disp('c')  
      elseif A(1,k) >= -55 && A(1,k)<= -48
       disp('D') 
-     end
+      elseif A(1,k) >= -48 && A(1,k)<= -43
+      disp('E') 
+      elseif A(1,k) >= -43 && A(1,k)<= -37
+      disp('F') 
+      elseif A(1,k) >= -37 && A(1,k)<= -30
+      disp('G') 
+      elseif A(1,k) >= -37 && A(1,k)<= -30
+      disp('A') 
+      
+       elseif A(1,k) >= -30 && A(1,k)<= -25
+      disp('B') 
+       elseif A(1,k) >= -25 && A(1,k)<= -20
+      disp('C') 
+       elseif A(1,k) >= -20 && A(1,k)<= -13
+      disp('D') 
+       elseif A(1,k) >= -13 && A(1,k)<= -8
+      disp('E') 
+      elseif A(1,k) >= -8 && A(1,k)<= 0
+      disp('F') 
+      elseif A(1,k) >= 0 && A(1,k)<= 3
+      disp('G') 
+      elseif A(1,k) >= 3 && A(1,k)<= 8
+      disp('A') 
+      
+     end   
 end
 
 
